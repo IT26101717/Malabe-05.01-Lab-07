c3f56a17001a5f65bf8b3f65d3f9014ef7600b3f
IT26101717 <it26101717@my.sliit.lk>
2026-09-16 08:08:55 +0530

    Add files via upload

---
+++ Q2/IT26101717Lab7Q2A.java
@@ -0,0 +1,13 @@
+public class IT26101717Lab7Q2A {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 5; i++) {
+            
+            for (int j = 0; j < 5; j++) {
+                System.out.print("$ ");
+            }
+            System.out.println(); // Move to next line
+        }
+    }
+}
+
